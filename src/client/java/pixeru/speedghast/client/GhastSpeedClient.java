c01e522ff9b4eb82fc3dbef47dbaca65d4adf8a8
noobnap1 <trulyrellymagma@gmail.com>
2026-09-16 17:44:43 +0600

    GhastSpeed

---
+++ src/client/java/pixeru/speedghast/client/GhastSpeedClient.java
@@ -0,0 +1,10 @@
+package pixeru.speedghast.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GhastSpeedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
